33d200fcadfa761a90211095fa83d4e01eeeccc1
Yuriy Hanysh <yuriy.hanysh@grammarly.com>
2022-07-27 18:46:19 +0200

    Initial commit

---
+++ stages/swift/src/Comedian.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+class Comedian {
+    func sayJoke() {
+        // TODO(yuriy): Implement the joke generation from the resources
+        print("- Knock knock!")
+        print("- Who's there?")
+        print("- Tank.")
+        print("- Tank who?")
+        print("- You’re welcome!")
+    }
+}
